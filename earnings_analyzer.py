cd10bcfc5e8a40ddcb4164dfa06cb50494b6ab19
adilshivakumar <66530835+adilshivakumar@users.noreply.github.com>
2021-09-09 17:06:43 -0700

    Add files via upload

---
+++ earnings_analyzer.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# TODO: Figure out how to make sure that this file uses the directory of the script location
+# so that it is more portable
+df = pd.read_csv (r'./input/Dataframe1.csv')
+print(df)
