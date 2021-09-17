27b20024b0a827520893f2f511a40fb5f68cb847
Adil Shivakumar <adilshivakumar@gmail.com>
2021-09-16 22:02:23 -0700

    Finished Dataframe 2a and 2b

--- projects/earnings_analyzer/__init__.py
+++ projects/earnings_analyzer/__init__.py
@@ -5,11 +5,12 @@ def std():
     # TODO: Figure out how to make sure that this file uses the directory of the script location
     # so that it is more portable
     df = pd.read_csv(r'./input/Dataframe1.csv')
+    df_step = df.T
     df2 = pd.read_csv(r'./input/Dataframe2b.csv')
     df_saved = df.drop(['0','1','2','3','4','5','6','7'], axis = 1)
     df_saved = df_saved.T
-    df_2a = df['-1'] - df['-30']
     std_dev = df_saved.std()
+    df_2a = (df_step.iloc[30] - df_step)/std_dev
     print(df)
     print(std_dev)
     print(df2)
